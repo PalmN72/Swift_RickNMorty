def1b387b7532ecbb8a467264d2b4747e423d746
Palmn72 <cs.palmqvist@gmail.com>
2023-11-15 16:21:04 +0100

    Initial Commit

---
+++ SwiftRickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftRickNMorty
+//
+//  Created by Salt Dev on 2023-11-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
