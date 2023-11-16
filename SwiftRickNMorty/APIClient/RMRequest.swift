bbdf78370c97672381ee9ce3119c7d514e0562e6
Palmn72 <cs.palmqvist@gmail.com>
2023-11-16 13:13:33 +0100

    structuring RMRequest

--- SwiftRickNMorty/APIClient/RMRequest.swift
+++ SwiftRickNMorty/APIClient/RMRequest.swift
@@ -8,4 +8,51 @@
 import Foundation
 
 /// Object which represents single API Call
-final class RMRequest {}
+final class RMRequest {
+    // Base url
+    // Endpoint
+    // Path components
+    // Query params
+    
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else {return nil}
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            string += argumentString
+        }
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
+}
